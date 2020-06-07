1ab75c8c4b4094d02a7398ed15df045472723be6
Pham Cao Dai <daikadai1026@gmail.com>
2020-06-07 14:33:38 +0700

    14. Routing Configuration

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,27 +1,42 @@
-import React, { Component } from 'react'
-import { Menu, Container, Button } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
+          <Menu.Item as={NavLink} exact to="/" header>
             <img src="assets/logo.png" alt="logo" />
             Re-vents
           </Menu.Item>
-          <Menu.Item name="Events" />
+          <Menu.Item as={NavLink} to="/events" name="Events" />
+          <Menu.Item as={NavLink} to="/people" name="People" />
           <Menu.Item>
-            <Button floated="right" positive inverted content="Create Event" />
+            <Button
+              as={Link}
+              to='/createEvent'
+              floated="right"
+              positive
+              inverted
+              content="Create Event"
+            />
           </Menu.Item>
           <Menu.Item position="right">
             <Button basic inverted content="Login" />
-            <Button basic inverted content="Sign Out" style={{marginLeft: '0.5em'}} />
+            <Button
+              basic
+              inverted
+              content="Sign Out"
+              style={{ marginLeft: "0.5em" }}
+            />
           </Menu.Item>
         </Container>
       </Menu>
-    )
+    );
   }
 }
 
-export default NavBar
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
-  ReactDOM.render(<App/>, rootEl)
+  ReactDOM.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  , rootEl)
 }
 
 if(module.hot) {
@@ -16,12 +21,5 @@ if(module.hot) {
   })
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+render();
 serviceWorker.unregister();
